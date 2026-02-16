4e762fe366032c2f771fe37c894a45577ebfb719
Dysofer <dylanalexander.solisfernandez.27@gmail.com>
2026-02-16 11:58:45 -0500

    LocalStorage y SessionStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,33 @@
 
 const x = document.getElementById("result");
-if (typeof(Storage) !== "undefined") {
-  x.innerHTML = "Your browser supports Web storage!";
-} else {
-  x.innerHTML = "Sorry, no Web storage support!";
+
+window.onload = function() {
+  if (typeof(Storage) !== "undefined") {
+    x.innerHTML = "Your browser supports Web storage!";
+    // Load saved values
+    var localName = localStorage.getItem('nameLocal');
+    if (localName) {
+      document.getElementById("localSpan").innerText = "Nombre guardado es: " + localName;
+    }
+    var sessionName = sessionStorage.getItem('nameSession');
+    if (sessionName) {
+      document.getElementById("sessionSpan").innerText = "Nombre guardado es: " + sessionName;
+    }
+  } else {
+    x.innerHTML = "Sorry, no Web storage support!";
+  }
 }
 
 function saveToLocalStorage() {
-
   var name = document.getElementById("nameLocal").value;
+  localStorage.setItem('nameLocal', name);
+  document.getElementById("localSpan").innerText = "Nombre guardado es: " + name;
   alert(name);
-
 }
 
 function saveToSessionStorage() {
-
   var name = document.getElementById("nameSession").value;
+  sessionStorage.setItem('nameSession', name);
+  document.getElementById("sessionSpan").innerText = "Nombre guardado es: " + name;
   alert(name);
 }
