389507daf272bf06c08f882b5d73b78c9da28906
Dysofer <dylanalexander.solisfernandez.27@gmail.com>
2026-02-23 11:19:16 -0500

    Create libraby.js

---
+++ src/js/library.js
@@ -0,0 +1,8 @@
+// Prestamo de libros
+// Funcion que se llame procesar solicitud
+//procesarSolicitud (nombreUsuario, libro1)
+//Sacar el nombre del usuario como primer elemento
+//Añadir al inicio del array la cadena "Carne de socio", ya que se entrega con cada solicitud
+//Añadir al final del array el nombre del usuario, el libro solicitado y la fecha de solicitud
+//Devolver el array modificado 
+
