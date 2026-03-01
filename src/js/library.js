5f67ef27efc19e52061ad4e6558a2164cdb89f60
Dysofer <dylanalexander.solisfernandez.27@gmail.com>
2026-03-01 14:32:13 -0500

    Create library.js y puse a funcionar la funcion de procesar

--- src/js/library.js
+++ src/js/library.js
@@ -6,3 +6,33 @@
 //Añadir al final del array el nombre del usuario, el libro solicitado y la fecha de solicitud
 //Devolver el array modificado 
 
+// =======================================
+// PRÉSTAMO DE LIBROS
+// =======================================
+
+function procesarSolicitud(arr) {
+
+    // 1. Sacar el nombre del usuario (primer elemento)
+    let nombreUsuario = arr[0];
+
+    // 2. Añadir al inicio del array "Carné de socio"
+    arr.unshift("Carné de socio");
+
+    // 3. Añadir al final del array el nombre del usuario
+    arr.push(nombreUsuario);
+
+    // 4. Devolver el array modificado
+    return arr;
+}
+
+
+// =======================
+// PRUEBA DE LA FUNCIÓN
+// =======================
+
+let solicitud = ["Dylan", "El principito", "Cien años de soledad"];
+
+let resultado = procesarSolicitud(solicitud);
+
+console.log("Resultado de la solicitud:");
+console.log(resultado);
